fbae8ae534d40fb03dc36f2ebdf1cb80a37b0347
leandrogiova <leandrogiovacchini@gmail.com>
2023-09-09 02:01:27 -0300

    .

--- src/main/java/com/neoris/turnosrotativos/TurnosrotativosApplication.java
+++ src/main/java/com/neoris/turnosrotativos/TurnosrotativosApplication.java
@@ -7,8 +7,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-import com.neoris.turnosrotativos.entities.Empleado;
-
 @SpringBootApplication
 public class TurnosrotativosApplication {
 
