6f113d16115cceeef544e7ff4437fd0f0fda057d
leandrogiova <leandrogiovacchini@gmail.com>
2023-09-11 06:51:12 -0300

    Trabajo terminado y probado

--- src/main/java/com/neoris/turnosrotativos/repositorys/EmpleadoRepository.java
+++ src/main/java/com/neoris/turnosrotativos/repositorys/EmpleadoRepository.java
@@ -3,6 +3,7 @@ package com.neoris.turnosrotativos.repositorys;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+
 import org.springframework.stereotype.Repository;
 
 import com.neoris.turnosrotativos.entities.Empleado;
